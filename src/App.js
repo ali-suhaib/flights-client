138c3499709e3c30a542b2ef52ce6b81c9107e98
ali <alisuhaib88@gmail.com>
2022-01-22 15:43:54 +0500

    Basic react structure

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { Route,Routes, Router } from 'react-router-dom';
+
+import Airline from './app/Airline/Airline';
+import Airlines from './app/Airlines/Airlines';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <Routes>
+        <Route exact path="/airline/:slug" element={<Airline />}/>
+        <Route exact path="/" element={<Airlines />}/>
+      </Routes>
+    </React.Fragment>
   );
 }
 
